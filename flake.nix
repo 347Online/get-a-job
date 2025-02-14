99e77169c139c3dd2bb159262000932aafdae741
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-14 14:59:52 -0600

    Configure Dioxus-cli and rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,22 +2,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      self,
+    inputs@{
       nixpkgs,
       utils,
+      ...
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        rust =
+          with inputs.fenix.packages.${system};
+          combine [
+            complete.toolchain
+            targets.wasm32-unknown-unknown.latest.rust-std
+          ];
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          packages = with pkgs; [
+            rust
             dioxus-cli
           ];
         };
