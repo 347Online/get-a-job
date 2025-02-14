9dc6fcde6f193f6e7837f9607dbe3d17b9caf36a
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-14 16:03:55 -0600

    Follow Dioxus tutorial up to Adding a backend
    Style changes (copied from tutorial)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use dioxus::prelude::*;
+use dioxus::{logger::tracing, prelude::*};
+use serde::Deserialize;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -11,27 +10,45 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-
+        document::Stylesheet { href: CSS}
+        Title {}
+        DogView {}
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
+fn Title() -> Element {
     rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+        div { id: "title",
+            h1 { "HotDog! 🌭"}
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct DogApi {
+    message: String,
+}
+
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    rsx! {
+        div { id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default() }
+        }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip!" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
         }
     }
 }
